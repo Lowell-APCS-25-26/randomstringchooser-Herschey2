668495053ac112c7afc48bd5b729004f7dca2a9b
Herschey2 <96698273+Herschey2@users.noreply.github.com>
2026-04-23 16:32:52 +0000

    done

--- lib/src/main/java/org/APCSLowell/RandomLetterChooser.java
+++ lib/src/main/java/org/APCSLowell/RandomLetterChooser.java
@@ -1,4 +1,12 @@
 package org.APCSLowell;
 
 // FRQ: https://secure-media.collegeboard.org/digitalServices/pdf/ap/ap16_frq_computer_science_a.pdf
-public class RandomLetterChooser extends RandomStringChooser {}
+public class RandomLetterChooser extends RandomStringChooser {
+    public RandomLetterChooser(String str){
+        super(getSingleLetters(str));
+    }
+    public static String[] getSingleLetters(String str){
+        return str.split("");
+    }
+
+}
--- lib/src/main/java/org/APCSLowell/RandomStringChooser.java
+++ lib/src/main/java/org/APCSLowell/RandomStringChooser.java
@@ -1,4 +1,18 @@
 package org.APCSLowell;
 
 // FRQ: https://secure-media.collegeboard.org/digitalServices/pdf/ap/ap16_frq_computer_science_a.pdf
-public class RandomStringChooser {}
+public class RandomStringChooser {
+    private ArrayList<String> vals;
+    public RandomStringChooser(String[] values){
+        vals = new ArrayList<String>();
+        for(String val : values){
+            vals.add(val);
+        }
+    }
+    public String getNext(){
+        if(vals.size()==0){
+            return "NONE";
+        }
+        return vals.remove((int) (Math.random()*vals.size()));
+    }
+}
